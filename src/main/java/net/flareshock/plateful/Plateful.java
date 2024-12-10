5e82c48fcfdc054e983c9a449bccfa9d23cad6fd
AlanListDev <alansedmkstreams@email.cz>
2024-12-10 09:09:38 +0100

    Initial commit

---
+++ src/main/java/net/flareshock/plateful/Plateful.java
@@ -0,0 +1,15 @@
+package net.flareshock.plateful;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Plateful implements ModInitializer {
+	public static final String MOD_ID = "plateful";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/flareshock/plateful/PlatefulClient.java
@@ -0,0 +1,10 @@
+package net.flareshock.plateful;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlatefulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
